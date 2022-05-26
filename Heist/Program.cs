f302cf9988657ad3463eb4fc384808861fa21780
Nayhee <nathantraczewski@gmail.com>
2022-05-26 13:47:21 -0500

    finished stages 1 and 2

--- Heist/Program.cs
+++ Heist/Program.cs
@@ -13,44 +13,37 @@ namespace Heist
 
             AddNewMember();
             DisplayMemberList();
-
-
-
-            void DisplayMemberList()
-            {
-                Console.WriteLine("Member List:");
-
-                foreach(Member mem in MemberList)
-                {
-                    Console.WriteLine($"{mem.Name}");
-                }
-            }
-
-            
+            DisplayNumOfMembers();
 
             void AddNewMember()
             {
                 Console.WriteLine("Please enter your team member's name");
                 string MemberNameFromUser = Console.ReadLine();
-                Console.WriteLine("Please enter your team member's skill level");
-                int SkillLevelFromUser = int.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter your team member's courage factor (0.0 - 2.0)");
-                decimal CourageFromUser = decimal.Parse(Console.ReadLine());
 
-                Member member = new Member(MemberNameFromUser, SkillLevelFromUser, CourageFromUser);
-                MemberList.Add(member);
-
-                Console.WriteLine("---------------------------------------------------------------------");
-                Console.WriteLine($"{member.Name} has a skill level of {member.SkillLevel} and a courage factor of {member.CourageFactor}.");
-                Console.WriteLine("---------------------------------------------------------------------");
-            
-               
-
-                Console.Write("Want to add another Team mate? (y/n)  ");
-                string Response = Console.ReadLine();
-                if(Response.ToLower() == "y")
+                if(MemberNameFromUser != "")
                 {
-                    AddNewMember();
+                    Console.WriteLine("Please enter your team member's skill level");
+                    int SkillLevelFromUser = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter your team member's courage factor (0.0 - 2.0)");
+                    decimal CourageFromUser = decimal.Parse(Console.ReadLine());
+
+                    Member member = new Member(MemberNameFromUser, SkillLevelFromUser, CourageFromUser);
+                    MemberList.Add(member);
+
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    Console.WriteLine($"{member.Name} has a skill level of {member.SkillLevel} and a courage factor of {member.CourageFactor}.");
+                    Console.WriteLine("---------------------------------------------------------------------");
+                
+                    Console.Write("Want to add another Team mate? (y/n)  ");
+                    string Response = Console.ReadLine();
+                    if(Response.ToLower() == "y")
+                    {
+                        AddNewMember();
+                    }
+                    else
+                    {
+                        return;
+                    }
                 }
                 else
                 {
@@ -58,6 +51,20 @@ namespace Heist
                 }
             }
 
+            void DisplayNumOfMembers()
+            {
+                Console.WriteLine($"Numer of Members: {MemberList.Count}");
+            }
+
+            void DisplayMemberList()
+            {
+                Console.WriteLine("Member List:");
+
+                foreach(Member mem in MemberList)
+                {
+                    Console.WriteLine($"{mem.Name} has a skill level of {mem.SkillLevel} and a courage factor of {mem.CourageFactor}");
+                }
+            }
             
         
         }
